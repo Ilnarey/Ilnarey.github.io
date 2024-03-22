8f83a652e8bc95d417c1f35426f9d8234230018c
Ilnarey <163994910+Ilnarey@users.noreply.github.com>
2024-03-22 12:42:57 +0500

    Delete start-clean directory
    Signed-off-by: Ilnarey <163994910+Ilnarey@users.noreply.github.com>

--- start-clean/js/main.js
+++
@@ -1,6 +0,0 @@
-var menuact = document.querySelector('.btn');
-var menuList = document.querySelector('.mens__list');
-
-menuact.addEventListener('click', ()=>{
-    menuList.classList.toggle('mens__list__open')
-});
